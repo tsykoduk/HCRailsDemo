a5960de6e1bebab2936c202e93682f3f669c4640
Greg Nokes <tsykoduk@gmail.com>
2017-10-09 13:45:18 -0700

    Add in GUID generator on record create

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,12 @@
 class Account < ActiveRecord::Base
   self.table_name = "salesforce.account"
+  after_create_commit :assign_uuid
+   
+   def assign_uuid
+     uuid = SecureRandom.uuid + Time.now().to_i.to_s
+     Self.external_id__c = uuid
+     Self.save
+   end
+   
   
 end
