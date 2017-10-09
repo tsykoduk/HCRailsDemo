30ae38772bc6a9ed1a6fec614bbf39450647a536
Greg Nokes <tsykoduk@gmail.com>
2017-10-09 15:07:50 -0700

    let's try and inject the uuid before the create

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ActiveRecord::Base
   self.table_name = "salesforce.account"
-  after_create_commit :assign_uuid
+  before_create_commit :assign_uuid
    
    def assign_uuid
      uuid = SecureRandom.uuid + Time.now().to_i.to_s
