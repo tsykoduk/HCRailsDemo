ad313980893826a36770ea30178d97be43d15267
Greg Nokes <tsykoduk@gmail.com>
2018-03-20 14:48:41 -0700

    move the loader out of the model into the helper

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,2 +1,24 @@
 module AccountHelper
+  
+  def loader (number)
+    number.times do
+	    acct = Account.new
+	    acct.name = Faker::Company.name
+      acct.billingcountry = Faker::Address.country
+	    acct.billinglatitude = Faker::Address.latitude
+      acct.description = Faker::Company.catch_phrase
+	    acct website = "http://example.com"
+	    acct.billinglongitude Faker::Address.longitude
+	    acct.billingstate Faker::Address.state
+	    acct.billingstreet = Faker::Address.street_address
+	    acct.tickersymbol Faker::Space.agency_abv
+	    acct.billingcity = Faker::Address.city
+ 	    acct.billingpostalcode = Faker::Address.postcode
+	    acct.phone = Faker::PhoneNumber.phone_number
+	    acct.fax = Faker::PhoneNumber.phone_number
+      acct.save!
+    end
+  end
+
+
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -3,27 +3,6 @@ class Account < ActiveRecord::Base
   before_validation :assign_uuid
    
    
-  def loader (number)
-   number.times do
-	  acct = Account.new
-	  acct.name = Faker::Company.name
-      acct.billingcountry = Faker::Address.country
-	  acct.billinglatitude = Faker::Address.latitude
-      acct.description = Faker::Company.catch_phrase
-	  acct website = "http://example.com"
-	  acct.billinglongitude Faker::Address.longitude
-	  acct.billingstate Faker::Address.state
-	  acct.billingstreet = Faker::Address.street_address
-	  acct.tickersymbol Faker::Space.agency_abv
-	  acct.billingcity = Faker::Address.city
- 	  acct.billingpostalcode = Faker::Address.postcode
-	  acct.phone = Faker::PhoneNumber.phone_number
-	  acct.fax = Faker::PhoneNumber.phone_number
-      acct.save!
-    end
-  end
-   
-   
   private
    def assign_uuid
       #TODO: Check to see if a UUID exists before creating one
