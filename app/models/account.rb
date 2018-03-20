26b18baacc3de28e64a4fa0f2f1cba67468c796b
Greg Nokes <greg@nokes.name>
2018-03-20 11:55:59 -0700

    adding loader logic

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,12 +3,31 @@ class Account < ActiveRecord::Base
   before_validation :assign_uuid
    
    
+  def loader (number)
+   number.times do
+	  acct = Account.new
+	  acct.name = Faker::Company.name
+      acct.billingcountry = Faker::Address.country
+	  acct.billinglatitude = Faker::Address.latitude
+      acct.description = Faker::Company.catch_phrase
+	  acct website = "http://example.com"
+	  acct.billinglongitude Faker::Address.longitude
+	  acct.billingstate Faker::Address.state
+	  acct.billingstreet = Faker::Address.street_address
+	  acct.tickersymbol Faker::Space.agency_abv
+	  acct.billingcity = Faker::Address.city
+ 	  acct.billingpostalcode = Faker::Address.postcode
+	  acct.phone = Faker::PhoneNumber.phone_number
+	  acct.fax = Faker::PhoneNumber.phone_number
+      acct.save!
+    end
+  end
    
    
   private
    def assign_uuid
      self.external_id__c = SecureRandom.uuid + Time.now().to_i.to_s
    end
-   
+  
   
 end
